77161a81694e06327ac79ddebc594833866eeafe
benke <info@typomotor.de>
2023-03-06 21:21:43 +0100

    New version 11.5.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'News Jobs',
 	'description' => 'Extends the Extension news with additional fields for jobs offers.',
 	'category' => 'fe',
-	'version' => '10.4.0',
+	'version' => '11.5.1',
 	'state' => 'stable',
 	'author' => 'Peter Benke',
 	'author_email' => 'info@typomotor.de',
 	'author_company' => '',
 	'constraints' =>[
 		'depends' => [
-			'typo3' => '10.4.0-10.4.99',
-			'news' => '8.0.0-8.9.99',
+			'typo3' => '11.5.0-11.5.99',
+			'news' => '10.0.0-10.9.99',
 		],
 	],
 );
